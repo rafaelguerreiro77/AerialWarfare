b3789f5cda2a44f771095ac8680f251a599d92d8
Rafael Guerreiro <raguerreiro77@gmail.com>
2025-10-14 08:36:32 -0300

    aula1

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode(size=(600, 480))
+print('Setup end')
+
+while True:
+    # check por todos os eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()  # close window
+            quit()  # end pygame
